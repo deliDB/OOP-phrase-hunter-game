ec344db4b6fd5a48ceb26883643ae22098260c68
Deli B <106265601+deliDB@users.noreply.github.com>
2022-07-03 18:55:09 -0700

    Update app.js

--- OOP-phrase-hunter-project/js/app.js
+++ OOP-phrase-hunter-project/js/app.js
@@ -9,51 +9,50 @@ const buttons = document.querySelectorAll('.key');
 const h1 = document.querySelector('h1');
 
 //Event listener for the "Start Game" button which creates a new Game object and starts the game by calling the startGame() method.
-startButton.addEventListener('click', function(){
-    game = new Game();
-    resetGame();
-    game.startGame();
+startButton.addEventListener('click', function() {
+	game = new Game();
+	resetGame();
+	game.startGame();
 });
 
 /*
-* Resets gameboard between games.
-*/ 
+ * Resets gameboard between games.
+ */
 
 const resetGame = () => {
-    const ul = document.querySelector('ul');
-    const lostHearts = document.querySelectorAll('img[src="images/lostHeart.png"]');
-    const h1Sibling = h1.nextElementSibling;
+	const ul = document.querySelector('ul');
+	const lostHearts = document.querySelectorAll('img[src="images/lostHeart.png"]');
+	const h1Sibling = h1.nextElementSibling;
 
-    ul.innerHTML = "";
-    h1Sibling.innerHTML = "";
-    startButton.innerText = 'Start Game';
+	ul.innerHTML = "";
+	h1Sibling.innerHTML = "";
+	startButton.innerText = 'Start Game';
 
-    [...buttons].forEach(button => {
-        button.disabled = '';
-        button.className = 'key';
-    });
+	[...buttons].forEach(button => {
+		button.disabled = '';
+		button.className = 'key';
+	});
 
-    [...lostHearts].forEach(heart => {
-        heart.attributes.src.textContent = 'images/liveHeart.png';
-    });
+	[...lostHearts].forEach(heart => {
+		heart.attributes.src.textContent = 'images/liveHeart.png';
+	});
 
-
-}
+};
 
 //Event listener for each of the onscreen keyboard buttons, so that clicking a button calls the handleInteraction() method on the Game object. 
 keyboard.addEventListener('click', (e) => {
-    const button = e.target;
-    if(button.tagName === 'BUTTON'){
-        game.handleInteraction(button);
-    }
+	const button = e.target;
+	if (button.tagName === 'BUTTON') {
+		game.handleInteraction(button);
+	}
 });
 
 document.addEventListener('keyup', (e) => {
-    const key = e.key;
-    const buttons = document.querySelectorAll('.key');
-    [...buttons].forEach(button => {
-        if(button.innerText === key){
-        game.handleInteraction(button);
-        }  
-    });
+	const key = e.key;
+	const buttons = document.querySelectorAll('.key');
+	[...buttons].forEach(button => {
+		if (button.innerText === key) {
+			game.handleInteraction(button);
+		}
+	});
 });
